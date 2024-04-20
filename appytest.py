796a635e77c35361b30acc0674552809e13a0ce7
RSkrep <robert.p.skrepnek@gmail.com>
2024-04-20 19:57:56 -0400

    update

--- appytest.py
+++ appytest.py
@@ -13,7 +13,7 @@ import pickle
 #################################################
 # Database Setup
 #################################################
-connection_string = "mongodb+srv://maisie:RTQH6l0wjghKk637@cluster0.9gjuly6.mongodb.net/mydatabase"
+connection_string = f"mongodb+srv://{mongo_username}:{mongo_password}@cluster0.9gjuly6.mongodb.net/mydatabase"
 
 # Create the MongoClient instance with SSL/TLS options
 mongo = MongoClient(connection_string)
@@ -128,5 +128,26 @@ def weather():
         output.append(x)
     return json.loads(json_util.dumps(output))
 
+@app.route('/affordable_housing')
+def affordable_hosuing():
+
+    return render_template('AffordableHousing.html')
+
+
+@app.route('/api/v1.0/predictions/<lat>/<lon>/<floor>/<beds>/<baths>/<garage>/<price>/<condo>/<det>/<townh>/<other>')
+def predictions(lat, lon, floor,beds,baths,garage,price,condo,det,townh,other):
+        #http://127.0.0.1:5000/api/v1.0/predictions/-78.891020/43.943300/499/0/1/0/200000/1/0/0/0 test this link
+        with open('Scaler.pk1','rb') as f:
+            scaler = pickle.load(f)
+            new_model = tf.keras.models.load_model('Neural_Network.h5')
+            X_new = np.array([float(lat),float(lon),int(floor),int(beds),int(baths),int(garage),int(price),int(condo),int(det),int(townh),int(other)])
+            X_new_scaled =  scaler.transform(X_new.reshape(1,11))
+            prediction = new_model.predict(X_new_scaled) 
+            prediction = np.where(prediction > 0.5,1,0)
+            output = [int(i) for i in prediction]
+            response = {
+                'prediction' :output
+            }
+            return (jsonify(response))  
 if __name__ == '__main__':
     app.run(debug=True)
