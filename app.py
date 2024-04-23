d3ae8861978b206254df9f9dab9428005d448f98
RSkrep <robert.p.skrepnek@gmail.com>
2024-04-22 20:06:50 -0400

    update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from pymongo import MongoClient
 from pprint import pprint
 from bson import json_util
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 import json
 from api_keys import mongo_username,mongo_password
 import ssl
@@ -28,6 +28,7 @@ all_houses = db["all_houses"]
 leisure_spaces=db['leisure_spaces']
 sold_houses = db['sold_houses']
 weather_data = db['weather_data']
+
 #################################################
 # Flask Setup
 #################################################
@@ -41,28 +42,32 @@ app = Flask(__name__)
     #Below I am taking a random index.html file to test
 @app.route("/")
 def main():
-    return (render_template('index.html'))
+    return (render_template('landing.html', is_landing=True))
+
+@app.route('/AffordableHousing.html')
+def affordable_housing():
+    return (render_template('AffordableHousing.html'))
 
 #2.API Page
     # the below json_util formula came from stack overflow https://stackoverflow.com/questions/16586180/typeerror-objectid-is-not-json-serializable
-    
+
 @app.route("/api/v1.0/housing")
 def api_data():
     query = {"city":{"$in":['Oshawa','Oakville','Vaughan','Milton','Burlington']}}
     results = all_houses.find(query)
     output = []
     for x in results:
-        output.append(x) 
-    return jsonify(json.loads(json_util.dumps(output)))
-                
+        output.append(x)
+    return json.loads(json_util.dumps(output))
+
 @app.route("/api/v1.0/oakville")
 def api_oakville():
     query = {"city":'Oakville'}
     results = all_houses.find(query)
     output = []
     for x in results:
-        output.append(x) 
-    return jsonify(json.loads(json_util.dumps(output)))
+        output.append(x)
+    return json.loads(json_util.dumps(output))
 
 @app.route("/api/v1.0/oshawa")
 def api_oshawa():
@@ -70,8 +75,8 @@ def api_oshawa():
     results = all_houses.find(query)
     output = []
     for x in results:
-        output.append(x) 
-    return jsonify(json.loads(json_util.dumps(output)))
+        output.append(x)
+    return json.loads(json_util.dumps(output))
 
 @app.route("/api/v1.0/milton")
 def api_milton():
@@ -79,8 +84,8 @@ def api_milton():
     results = all_houses.find(query)
     output = []
     for x in results:
-        output.append(x) 
-    return jsonify(json.loads(json_util.dumps(output)))
+        output.append(x)
+    return json.loads(json_util.dumps(output))
 
 @app.route("/api/v1.0/burlington")
 def api_burlington():
@@ -88,8 +93,8 @@ def api_burlington():
     results = all_houses.find(query)
     output = []
     for x in results:
-        output.append(x) 
-    return jsonify(json.loads(json_util.dumps(output)))
+        output.append(x)
+    return json.loads(json_util.dumps(output))
 
 @app.route("/api/v1.0/vaughan")
 def api_vaughan():
@@ -97,8 +102,8 @@ def api_vaughan():
     results = all_houses.find(query)
     output = []
     for x in results:
-        output.append(x) 
-    return jsonify(json.loads(json_util.dumps(output)))
+        output.append(x)
+    return json.loads(json_util.dumps(output))
 
 @app.route("/api/v1.0/leisure")
 def api_leisure():
@@ -106,8 +111,8 @@ def api_leisure():
     results = leisure_spaces.find(query)
     output = []
     for x in results:
-        output.append(x) 
-    return jsonify(json.loads(json_util.dumps(output)))
+        output.append(x)
+    return json.loads(json_util.dumps(output))
 
 @app.route("/api/v1.0/sold-houses")
 def api_sold_houses():
@@ -115,8 +120,8 @@ def api_sold_houses():
     results = sold_houses.find(query)
     output = []
     for x in results:
-        output.append(x) 
-    return jsonify(json.loads(json_util.dumps(output)))
+        output.append(x)
+    return json.loads(json_util.dumps(output))
 
 @app.route("/api/v1.0/weather")
 def weather():
@@ -124,8 +129,21 @@ def weather():
     results = weather_data.find(query)
     output = []
     for x in results:
-        output.append(x) 
-    return jsonify(json.loads(json_util.dumps(output)))
+        output.append(x)
+    return json.loads(json_util.dumps(output))
+
+@app.route("/api/v1.0/affordable_housing")
+def api_affordable_housing():
+    income = request.args.get('preTaxIncome', type=float)
+    status = request.args.get('status')
+    query = {"price": {"$lte": income}}
+    if status:
+        query["status"] = status
+    results = all_houses.find(query)
+    output = []
+    for x in results:
+        output.append(x)
+    return json.loads(json_util.dumps(output))
 
 @app.route('/api/v1.0/predictions/<lat>/<lon>/<floor>/<beds>/<baths>/<garage>/<price>/<condo>/<det>/<townh>/<other>')
 def predictions(lat, lon, floor,beds,baths,garage,price,condo,det,townh,other):
@@ -141,7 +159,7 @@ def predictions(lat, lon, floor,beds,baths,garage,price,condo,det,townh,other):
             response = {
                 'prediction' :output
             }
-            return (jsonify(response))     
+            return (jsonify(response))
 
 if __name__ == '__main__':
     app.run(debug=True)
